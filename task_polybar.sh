babd81295754b4a41b19bf5fb1bfddb0024e1dc3
Rexoen <34290284+Rexoen@users.noreply.github.com>
2022-04-14 18:35:32 +0800

    change the due time showing logic.
    no separator dot will be shown when the most urgent task has no due time.

--- task_polybar.sh
+++ task_polybar.sh
@@ -4,4 +4,10 @@ most_urgent_desc=`task rc.verbose: rc.report.next.columns:description rc.report.
 most_urgent_id=`task rc.verbose: rc.report.next.columns:id rc.report.next.labels:1 limit:1 next`
 most_urgent_due=`task rc.verbose: rc.report.next.columns:due.relative rc.report.next.labels:1 limit:1 next`
 echo "$most_urgent_id" > /tmp/tw_polybar_id
-echo "$most_urgent_desc ·  $most_urgent_due"
+if [ "$most_urgent_desc" != "" ];then
+    printf "$most_urgent_desc"
+fi
+
+if [ "$most_urgent_due" != "" ];then
+    printf " · $most_urgent_due"
+fi
